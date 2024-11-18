8cea690d3c512bc664423456b8c70a862a15d6d1
nson22 <glecinilson@gmail.com>
2024-11-18 17:39:13 -0400

    update login and logout

---
+++ deploy.py
@@ -0,0 +1,22 @@
+import pyautogui
+from time import sleep
+
+
+pyautogui.click(2300, 120)  # Start button
+sleep(1)
+pyautogui.click(2300, 230)  # Choose the game
+sleep(2)
+pyautogui.click(950, 570)  # Choose the game
+sleep(5)
+pyautogui.click(1635, 520)  # Choose the game
+sleep(2)
+pyautogui.click(1590, 1010)  # Choose the game
+sleep(3)
+pyautogui.click(1590, 1010)  # Choose the game
+
+# 2300, 120 
+# 2300, 230
+# 950, 570
+# 1635, 520
+# 1590, 1010
+# 1590, 1010
